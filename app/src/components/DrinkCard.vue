615277f21ccd4bf23f02e714acf77aa1185d961b
Juiceman78 <vincentz78@nycstudents.net>
2026-02-12 14:02:27 -0500

    Happy birthday Weiming Weng

---
+++ app/src/components/DrinkCard.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h2>{{ drink.name }}</h2>
+    <h3>{{ drink.price }}</h3>
+    <button @click="addCart">Add to Cart</button>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  drink: {
+    type: Object,
+    required: true,
+  },
+})
+</script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 200px;
+  width: 150px;
+  border: 3px solid black;
+}
+</style>
--- app/src/views/DrinkView.vue
+++ app/src/views/DrinkView.vue
@@ -1,33 +1,37 @@
 <template>
   <div>
     <h1>Drinks</h1>
-    <button @click="addCart" id="CMB">Add to Cart</button>
+    
+    <DrinkCard v-for="drink in drinks" :key="drink.name" :drink="drink" />
   </div>
 </template>
 
 <script setup>
-;[
-  { id: 'CCB', drink: 'Cloud Coconut Blue', price: 7.99 },
-  { id: 'TSML', drink: 'Triple Supreme Matcha Latte', price: 7.99 },
-  { id: 'SML', drink: 'Supreme Matcha Latte', price: 7.99 },
-  { id: 'CML', drink: 'Cloud Matcha Latte', price: 7.99 },
-  { id: 'JL', drink: 'Jasmine Latte', price: 6.49 },
-  { id: 'CMB', drink: 'Coconut Mango Boom', price: 6.99 },
-  { id: 'CGB', drink: 'Crisp Grape Boom', price: 7.49 },
-  { id: 'CJT', drink: 'Cloud Jasmine Tea', price: 5.49 },
-  { id: 'KBT', drink: 'Kale Boost Tea', price: 7.99 },
-  { id: 'SBSBMT', drink: 'Supreme Brown Sugar Bobo Milk Tea', price: 7.99 },
-  { id: 'BTL', drink: 'Black Tea Latte', price: 6.49 },
-  { id: 'JT', drink: 'Jasmine Tea', price: 3.99 },
-  { id: 'FCY', drink: 'Fluffy Cloud Yumberry', price: 9.99 },
-  { id: 'FCM', drink: 'Fluffy Cloud Mango', price: 9.99 },
-  { id: 'FCCG', drink: 'Fluffy Cloud Crisp Grape', price: 9.99 },
-  { id: 'YB', drink: 'Yumberry Boom', price: 7.49 },
-  { id: 'MB', drink: 'Mulberry Boom', price: 7.49 },
-  { id: 'PFB', drink: 'Passion Fruit Blast', price: 6.49 },
+import DrinkCard from '../components/DrinkCard.vue'
+const drinks = [
+  { id: 'CCB', name: 'Cloud Coconut Blue', price: 7.99 },
+  { id: 'TSML', name: 'Triple Supreme Matcha Latte', price: 7.99 },
+  { id: 'SML', name: 'Supreme Matcha Latte', price: 7.99 },
+  { id: 'CML', name: 'Cloud Matcha Latte', price: 7.99 },
+  { id: 'JL', name: 'Jasmine Latte', price: 6.49 },
+  { id: 'CMB', name: 'Coconut Mango Boom', price: 6.99 },
+  { id: 'CGB', name: 'Crisp Grape Boom', price: 7.49 },
+  { id: 'CJT', name: 'Cloud Jasmine Tea', price: 5.49 },
+  { id: 'KBT', name: 'Kale Boost Tea', price: 7.99 },
+  { id: 'SBSBMT', name: 'Supreme Brown Sugar Bobo Milk Tea', price: 7.99 },
+  { id: 'BTL', name: 'Black Tea Latte', price: 6.49 },
+  { id: 'JT', name: 'Jasmine Tea', price: 3.99 },
+  { id: 'FCY', name: 'Fluffy Cloud Yumberry', price: 9.99 },
+  { id: 'FCM', name: 'Fluffy Cloud Mango', price: 9.99 },
+  { id: 'FCCG', name: 'Fluffy Cloud Crisp Grape', price: 9.99 },
+  { id: 'YB', name: 'Yumberry Boom', price: 7.49 },
+  { id: 'MB', name: 'Mulberry Boom', price: 7.49 },
+  { id: 'PFB', name: 'Passion Fruit Blast', price: 6.49 },
 ]
 
-function addCart() {}
+function addCart() {
+  console.log('Drink has been added')
+}
 </script>
 
 <style scoped></style>
